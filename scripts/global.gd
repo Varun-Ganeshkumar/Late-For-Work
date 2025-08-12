82ac4c793014546515a807ded76cdeb7c267292d
Varun-Ganeshkumar <varunganeshkumar14@gmail.com>
2025-08-12 12:25:34 -0400

    art and mechanics

---
+++ scripts/global.gd
@@ -0,0 +1,3 @@
+extends Node
+
+var coyote_time := true
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,24 +1,53 @@
 extends CharacterBody2D
 
 
-const SPEED = 200.0
+const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var coyote_time: Timer = $CoyoteTime
+@onready var slow_time: Timer = $SlowTime
 
 func _physics_process(delta: float) -> void:
+	var was_on_floor = is_on_floor()
 	# Add the gravity.
-	if not is_on_floor():
+	if not is_on_floor() && global.coyote_time == false:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and (is_on_floor() || global.coyote_time):
 		velocity.y = JUMP_VELOCITY
-
+		if global.coyote_time:
+			global.coyote_time = false
+	# Main time mechanic
+	if Input.is_action_just_pressed("slow"):
+		Engine.time_scale = 2
+		slow_time.start()
+	if Engine.time_scale < 1:
+		Engine.time_scale = 1
 	# Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	# Flips sprite 
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true
 	move_and_slide()
+	if was_on_floor && !is_on_floor() && velocity.y >= 0:
+		global.coyote_time = true
+		coyote_time.start()
+# Adds coyote time for better movement
+func _on_coyote_time_timeout() -> void:
+	global.coyote_time = false
+
+func _on_slow_time_timeout() -> void:
+	if Engine.time_scale == 1:
+		pass
+	elif Engine.time_scale != 1:
+		Engine.time_scale = Engine.time_scale - 0.01
+		slow_time.start()
+	
