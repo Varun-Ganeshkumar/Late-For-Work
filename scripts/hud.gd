a1bf99afa23b54ae30db67d41493a425dc301f62
Varun-Ganeshkumar <varunganeshkumar14@gmail.com>
2026-06-30 15:07:23 -0400

    Made vignette passibly functional
    Still issues with it, and a want for a smoother transition

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,6 +1,20 @@
 extends Control
 
+@onready var border = $CanvasLayer/ColorRect
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	%Label.text = "ITEMS: \n Coffee: " + str(global.hc_coffee) + "\n Adrenaline: " + str(global.hc_adr)
+	%Label.text = "ITEMS: \n Coffee: " + str(global.hc_coffee) + "\n Adrenaline: " + str(global.hc_adr);
+	border.material.set_shader_parameter('alpha', abs(global.shader_mult));
+	
+	var color_change = 1.00
+	
+	if global.shader_mult < 0:
+		color_change = 1.00
+		border.material.set_shader_parameter("standard" , color_change);
+	elif global.shader_mult > 1:
+		color_change = 1.00
+		while color_change > 0:
+			await get_tree().create_timer(1).timeout
+			color_change = color_change - 0.01
+			border.material.set_shader_parameter("standard" , color_change);
