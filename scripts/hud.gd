d4852eeea4b2d7f5be8d20032053b1cba970d198
Varun-Ganeshkumar <varunganeshkumar14@gmail.com>
2026-07-01 14:23:04 -0400

    Fully fixed vignette and transition
    Took a long time, but that is now perfected in terms of code (maybe future edits for design but it depends how long everything else will take)

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,20 +1,22 @@
 extends Control
 
 @onready var border = $CanvasLayer/ColorRect
+var color_change = 1.00
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	%Label.text = "ITEMS: \n Coffee: " + str(global.hc_coffee) + "\n Adrenaline: " + str(global.hc_adr);
 	border.material.set_shader_parameter('alpha', abs(global.shader_mult));
-	
-	var color_change = 1.00
-	
-	if global.shader_mult < 0:
-		color_change = 1.00
+# IF COLOR CHANGE IS 1, THAT MEANS IT WILL TURN WHITE
+	if global.shader_mult > 0:
+		color_change = 0.00
 		border.material.set_shader_parameter("standard" , color_change);
-	elif global.shader_mult > 1:
-		color_change = 1.00
-		while color_change > 0:
-			await get_tree().create_timer(1).timeout
-			color_change = color_change - 0.01
-			border.material.set_shader_parameter("standard" , color_change);
+	if global.shader_mult < 0:
+		_colorTransition()
+			
+			
+func _colorTransition() -> void:
+	while color_change < 1:
+		await get_tree().create_timer(0.1).timeout
+		color_change = color_change + 0.01
+		border.material.set_shader_parameter("standard" , color_change);
