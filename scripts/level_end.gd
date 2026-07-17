d8261a9138e43423985d3304c90c3fe69a66c273
Varun-Ganeshkumar <varunganeshkumar14@gmail.com>
2026-07-17 12:59:12 -0400

    Finished the levels
    Made all of the levels and made it so when you finish them, you got unlimited coffee and double jumps no matter what mode you completed.

--- scripts/level_end.gd
+++ scripts/level_end.gd
@@ -2,5 +2,10 @@ extends Area2D
 
 
 func _on_body_entered(_body: Node2D) -> void:
+	queue_free()
+	Engine.time_scale = 1
+	global.shader_mult = 0
 	global.current_level += 1
+	global.hc_adr = 0
+	global.hc_coffee = 0 
 	get_tree().change_scene_to_file("res://scenes/level_" + str(global.current_level) +".tscn")
