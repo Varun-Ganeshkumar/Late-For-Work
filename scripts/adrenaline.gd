63bfdca3bd50407c5cc79f2d5d55e35eb8dd61ee
Varun-Ganeshkumar <varunganeshkumar14@gmail.com>
2026-07-17 12:58:32 -0400

    Fixed certain errors
    There used to be around 10 errors in Godot, this changed them making it no longer call the label every single time with Godot not knowing what it was

--- scripts/adrenaline.gd
+++ scripts/adrenaline.gd
@@ -2,5 +2,7 @@ extends Area2D
 
 
 func _on_body_entered(_body: Node2D) -> void:
-	global.hc_adr += 10
+	global.hc_adr += 1
 	queue_free()
+	if get_tree().current_scene.scene_file_path == "res://scenes/level_1.tscn":
+		$"../../Text/Label4".queue_free()
--- scripts/coffee.gd
+++ scripts/coffee.gd
@@ -1,6 +1,9 @@
 extends Area2D
 
 
+
 func _on_body_entered(_body: Node2D) -> void:
 	global.hc_coffee += 1
 	queue_free()
+	if get_tree().current_scene.scene_file_path == "res://scenes/level_1.tscn":
+		$"../../Text/Label2".queue_free()
